06d75c010dddef0ae3ef3a828b638770c151daf1
zach <zach@zachkamp.com>
2022-03-15 12:26:47 -0400

    update

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -27,40 +27,5 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-    h3 {
-        margin: 40px 0 0;
-    }
 
-    ul {
-        list-style-type: none;
-        padding: 0;
-    }
-
-    li {
-        display: inline-block;
-        margin: 0 10px;
-    }
-
-    a {
-        color: #42b983;
-    }
-
-    .heading-left {
-        font-size: 5rem;
-    }
-
-    .heading-left:hover {
-        background-color: #2c3e50;
-        color: #ededed;
-    }
-
-    /* section {
-      height: 100vh;
-    } */
-
-    @media (max-width: 575px) {
-        .heading-left {
-            font-size: 3.5rem;
-        }
-    }
 </style>
